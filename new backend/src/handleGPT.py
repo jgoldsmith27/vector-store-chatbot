ebd795bfb1043a719aa897a14964fcdd2f96b33d
jgoldsmith27 <jacob.goldsmith@gmail.com>
2024-12-09 13:38:35 -0500

    deleted old gpt api file

--- new backend/src/handleGPT.py
+++
@@ -1,8 +0,0 @@
-from openai import OpenAI
-
-class OpenAIClient:
-    def __init__(self, API_KEY:str, ASST_ID:str):
-        self.client = OpenAI(api_key=API_KEY)
-        self.asst_id = ASST_ID
-
-    

72d73fd95da0ea37f2ad6a8ae43465127e03cdd6
jgoldsmith27 <jacob.goldsmith@gmail.com>
2024-12-09 19:07:01 -0500

    deleted old backend

--- backend/routes/chatRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { handleChatRequest } = require("../controllers/chatController");
-
-const router = express.Router();
-
-router.post("/chat", handleChatRequest);
-
-module.exports = router;
